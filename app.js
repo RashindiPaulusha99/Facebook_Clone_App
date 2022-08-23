5c78507000be0ce9419d8c22b86b81df49abb732
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2022-08-23 07:43:21 +0530

    init the project

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app=express()
+const port = 4001
+
+const url = 'mongodb://127.0.0.1/facebookClone'
+
+mongoose.connect(url, { useNewUrlParser: true })
+const con = mongoose.connection
+
+con.on("open", () => {
+    console.log('MongoDB connected!');
+})
+
+app.use(express.json())
+
+/*app.get('/',(req,res)=>{
+    console.log('get request coming.');
+    res.send('get req came for /route');
+})*/
+
+app.listen(port, () => {
+    console.log(`app starting on ${port}`);
+})
